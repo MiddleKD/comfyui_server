0cb93930c4e43c3006fd5289bac92699f61653dc
ml-favorfit-develop <122414946+MiddleKD@users.noreply.github.com>
2024-06-27 17:08:25 +0900

    Update: server logging

--- async_server.py
+++ async_server.py
@@ -27,9 +27,10 @@ class BridgeServer():
         try:
             await self.sockets_res[sid].send_json(message)
             self.ws_connection_status[sid] = message.get("status", None)
+            logging.debug(f"[WS] SEND OK / {message} / {sid}")
         except Exception as err:
             self.ws_connection_status[sid] = "error"
-            logging.warning(f"send error: {err} / {message}")
+            logging.debug(f"[WS]] SEND FAILED / {err} / {message} / {sid}")
 
     async def init_app(self):
         app = web.Application(client_max_size=1024**2*100)  # 업로드 용량 확인 middlek
@@ -47,18 +48,21 @@ class BridgeServer():
     async def track_progress(self, sid):
         total_progress = 0
         cur_progress = 0
+
+        logging.info(f"[WS] TRACING START / {sid}")
         while True:
             out = await self.sockets_req[sid].receive()
             out = out.data
 
             if self.ws_connection_status[sid] == "closed" or self.ws_connection_status[sid] == "error":
-                # await self.sockets_req[sid].send_str("interrupt")
                 break
 
             if isinstance(out, str):
                 message = json.loads(out)
-
+                
                 if message['type'] == 'execution_start':
+                    logging.info(f"[WS] EXECUTION START / {sid}")
+
                     wf_info = self.wf_info.get(sid, None)
                     inputs_infos = [cur.get("inputs", None) for cur in wf_info.values()]
                     steps_list = [value for inputs_info in inputs_infos
@@ -88,6 +92,8 @@ class BridgeServer():
                     await self.send_socket_catch_exception(sid, progress_message)
 
                 if message['type'] == 'execution_cached':
+                    logging.debug(f"[WS] EXECUTION DONE / {sid}")
+                    
                     cached_nodes = message['data']['nodes']
                     cur_progress += len(cached_nodes)
                     progress_message = {
@@ -97,24 +103,31 @@ class BridgeServer():
                     await self.send_socket_catch_exception(sid, progress_message)
             else:
                 continue
+        logging.info(f"[WS] TRACING DONE / {sid}")
+
         return
 
 
     async def websocket_connection(self, request):
         sid = request.rel_url.query.get('clientId', '')
 
+        logging.info(f"[WS] RECEIVED / {sid}")
+        
         async with aiohttp.ClientSession() as session:
             server_address = await self.get_not_busy_server_address()
             self.sid_server_map[sid] = server_address
+            logging.debug(f"[WS] server allocated to {server_address} / {sid}")
             
             ws_res = web.WebSocketResponse()
             ws_req = await session.ws_connect(f"ws://{server_address}/ws?clientId={sid}")
+            logging.info(f"[WS] HANDSHAKE / {sid}")
 
             self.sockets_res[sid] = ws_res
             self.sockets_req[sid] = ws_req
 
             try:
                 await self.sockets_res[sid].prepare(request)
+                logging.info(f"[WS] BRIDGE CONNECTED / {sid}")
 
                 await self.send_socket_catch_exception(sid, {"status":"connected", "details":"web socket connected"})
                 self.ws_connection_status[sid] = "connected"
@@ -122,24 +135,32 @@ class BridgeServer():
                 task = asyncio.create_task(self.track_progress(sid))
 
                 timeout_count = 0
+                limit_timeout_count = 100
+                time_out_interval = 5
                 while True:
                     if self.ws_connection_status[sid] == "closed" or self.ws_connection_status[sid] == "error":
                         break
                     else:
                         await self.send_socket_catch_exception(sid, {"status":"listening", "details":"server is listening"})
-                        if timeout_count >= 500:
+                        if timeout_count >= limit_timeout_count:
                             raise TimeoutError(f"timeout count: {timeout_count}")
                         timeout_count += 1
 
-                    await asyncio.sleep(5)
+                    await asyncio.sleep(time_out_interval)
                 await task
 
             except aiohttp.ServerDisconnectedError as e:
+                logging.warning(f"[WS] SERVER DISCONNECTED ERROR / {sid}")
                 await self.send_socket_catch_exception(sid, {"status":"error", "details":"server disconnected"})
+            except TimeoutError as e:
+                logging.warning(f"[WS] TIMEOUT ERROR / {sid}")
+                await self.send_socket_catch_exception(sid, {"status":"error", "details":f"time out error: exceed {limit_timeout_count * time_out_interval}s"})
             except Exception as e:
+                logging.error(f"[WS] UNKNOWN ERROR / {sid}")
                 await self.send_socket_catch_exception(sid, {"status":"error", "details":str(e)})
 
             finally:
+                logging.info(f"[WS] CLOSED / {sid}")
                 await self.send_socket_catch_exception(sid, {"status":"closed", "details":"connection will be closed"})
                 await self.sockets_req[sid].close()
                 await self.sockets_res[sid].close()
@@ -153,8 +174,8 @@ class BridgeServer():
             try:
                 queue_state = get_queue_state(server_address)
             except Exception as e:
-                print(f"{server_address} in error {e}")
-                
+                logging.debug(f"[NO SIGNAL] {server_address} / {e}")
+
             queue_length = sum([len(cur) for cur in queue_state.values()])
             queue_lenghs.append(queue_length)
         
@@ -178,13 +199,27 @@ class BridgeServer():
     async def upload_image(self, request):
         reader = await request.multipart()
 
+        logging.info(f"[POST] '{request.path}'")
+
         fns={}
         async for part in reader:
-            file_name = part.headers.get('Content-Disposition', '').split('filename=')[1].strip('"')
-            file_data = await part.read()
+
+            try:
+                file_name = part.headers.get('Content-Disposition', '').split('filename=')[1].strip('"')
+                file_data = await part.read()
+            
+                fn = save_binary_file(file_data, file_name)
+                fns[part.headers.get("ori_file_id", None)] = fn
+                logging.debug(f"[POST] '{request.path}' / {fn} saved")
         
-            fn = save_binary_file(file_data, file_name)
-            fns[part.headers.get("ori_file_id", None)] = fn
+            except Exception as e:
+                logging.error(f"[POST] '{request.path}' / {file_name} can't save / {str(e)}")
+
+                return web.Response(
+                    status=400,
+                    body=json.dumps({"detail":f"{file_name} can't save"}),
+                    headers={"Content-Type": "application/json"}
+                )
 
         return web.Response(
             status=200,
@@ -196,7 +231,10 @@ class BridgeServer():
         sid = request.rel_url.query.get('clientId', '')
 
         history = get_history(sid, self.sid_server_map[sid])
+        logging.debug(f"[GET] '{request.path}' / GET HISTORY / {sid}")
         delete_history(sid, self.sid_server_map[sid])
+        logging.debug(f"[GET] '{request.path}' / DELETE HISTORY / {sid}")
+
         history = history.get(sid, None)
         self.sid_server_map.pop(sid, None)
 
@@ -225,14 +263,17 @@ class BridgeServer():
         else:
             return web.Response(
                 status=204,
-                text="No contents with that client id."
+                body=json.dumps({"detail":f"No contents with that client id / {sid}"}),
+                headers={"Content-Type": "application/json"}
             )
     
     async def workflow_info(self, request):
         data = await request.json()
-        
+
         workflow = data.pop("workflow", None)
         node_info = get_parsed_input_nodes(workflow)
+
+        logging.debug(f"[GET] '{request.path}' / GET PARSED WORKFLOW INFO")
 
         return web.Response(status=200, body=json.dumps(node_info), content_type="application/json")
     
@@ -250,7 +291,12 @@ def main():
     servers_str = os.getenv('COMFYUI_SERVERS')
     host = os.getenv("HOST")
     port = os.getenv("PORT")
+    logging_level = os.getenv("LOGGING_LEVEL", "INFO").upper()
 
+    logging.basicConfig(level=getattr(logging, logging_level, logging.INFO),
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        handlers=[logging.StreamHandler()])
+    
     server_list = servers_str.split(',') if servers_str else []
 
     loop = asyncio.get_event_loop()
