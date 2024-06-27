aa6aa30e4e157c94aafa21aaface5b442867556b
ml-favorfit-develop <122414946+MiddleKD@users.noreply.github.com>
2024-06-27 13:58:11 +0900

    trouble shoot

--- nodes.py
+++ nodes.py
@@ -1940,6 +1940,7 @@ def init_external_custom_nodes():
     node_import_times = []
     for custom_node_path in node_paths:
         possible_modules = os.listdir(os.path.realpath(custom_node_path))
+        print(possible_modules)
         if "__pycache__" in possible_modules:
             possible_modules.remove("__pycache__")
 
